fa195b0b8cd0cecbd1f069fbf304c22affe6176e
Rania Heni <72651840+raniaheni@users.noreply.github.com>
2022-04-13 23:42:23 +0100

    avion finalise

---
+++ cata.php
@@ -0,0 +1,137 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+    <title>URJET</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript>
+        <link rel="stylesheet" href="assets/css/noscript.css" />
+    </noscript>
+</head>
+
+<body class="landing is-preload">
+
+    <!-- Page Wrapper -->
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <header id="header" class="alt">
+            <h1><a href="index.php">URJET</a></h1>
+            <nav id="nav">
+                <ul>
+                    <li class="special">
+                        <a href="#menu" class="menuToggle"><span>Menu</span></a>
+                        <div id="menu">
+                            <ul>
+                                <li><a href="index.php">Accueil (airplanes catalog)</a></li>
+                                <li><a href="cata.php">Airplanes catalog</a></li>
+                                <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] && isset($_SESSION["Admin"]) && $_SESSION["Admin"] === true) { ?>
+                                    <li><a href="add.php">Avion</a></li>
+                                <?php } ?>
+                                <li><a href="Resersation.php"></a>Réservation</li>
+                                <li><a href="Reclamation.php">Réclamation</a></li>
+                                <li><a href="Reponse.php">Réponse</a></li>
+                                <li><a href="Maintenance.php"></a>Maitenance</li>
+                                <li><a href="Demande.php">Demande de maitenance</a></li>
+                                <li><a href="Voyage.php">Voyage</a></li>
+                                <?php if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) { ?>
+                                    <li><a href="signup.php">Sign Up</a></li>
+                                    <li><a href="login.php">Log In</a></li>
+                                <?php } ?>
+                                <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] && isset($_SESSION["Admin"]) && $_SESSION["Admin"] === true) { ?>
+                                    <li><a href="AdminPanel.php">Admin Panel</a></li>
+                                <?php } ?>
+                                <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
+                                    <li><a href="settings.php">Settings</a></li>
+                                    <li><a href="logout.php">Log out</a></li>
+                                <?php } ?>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- Banner -->
+        <section id="banner">
+            <div class="inner">
+                <h2>URJET</h2>
+                <p>Just a test<br />
+                    site template<br /></p>
+                <ul class="actions special">
+                    <li><a href="#" class="button primary">Activate</a></li>
+                </ul>
+            </div>
+            <a href="#one" class="more scrolly">Learn More</a>
+        </section>
+
+        <!-- One -->
+
+        <!-- Two -->
+        <section id="two" class="wrapper alt style2">
+            <?php
+            require_once "config.php";
+            $result = mysqli_query($con, "SELECT * FROM avion");
+
+            while ($row = mysqli_fetch_array($result)) {
+                //echo $row['FirstName'] . " " . $row['LastName']; //these are the fields that you have stored in your database table employee
+                echo "<br />";
+                ?>
+                <section class="spotlight">
+                <div class="image"><img src="imgair/<?php echo $row['photo'] ?>" alt="" /></div>
+                <div class="content">
+                    <h2><?php echo $row['nom'] ?><br />
+                    </h2>
+                    <h2><?php echo $row['type'] ?><br />
+                    </h2>
+                    <h2><?php echo $row['prix'] ?>DT<br />
+                    </h2>
+                </div>
+            </section>
+            <?php
+            }
+            ?>
+        </section>
+
+
+
+        <!-- Footer -->
+        <footer id="footer">
+            <ul class="icons">
+                <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+                <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+            </ul>
+            <ul class="copyright">
+                <li>&copy; Untitled</li>
+                <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            </ul>
+        </footer>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,185 @@
+<?php
+// Initialize the session
+session_start();
+ 
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] && isset($_SESSION["Admin"]) && $_SESSION["Admin"] === true){
+    header("location: index.php");
+    exit;
+}elseif(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] && isset($_SESSION["User"]) && $_SESSION["User"] === true){
+    header("location: index.php");
+    exit;
+}
+
+ 
+// Include config file
+require_once "config.php";
+$username = $password = "";
+$admin ="Admin";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        echo "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        echo "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, email, password, name, lastname, image ,id_o FROM user WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($con, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username,$email, $hashed_password,  $name, $lastname, $image,$id_o);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            $sqlo = "SELECT type , description FROM usero WHERE id = ?";
+                            $stmto = mysqli_prepare($con, $sqlo);
+                            mysqli_stmt_bind_param($stmto, "i", $id_o);
+                            if(mysqli_stmt_execute($stmto)){
+                            mysqli_stmt_store_result($stmto);
+                            mysqli_stmt_bind_result($stmto, $type, $description);
+                            mysqli_stmt_fetch($stmto);
+                            }
+                            session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+                            $_SESSION["email"] = $email;
+                            $_SESSION["hpassword"] = $hashed_password;
+                            $_SESSION["name"] = $name;
+                            $_SESSION["lastname"] = $lastname;
+                            $_SESSION["id_o"] = $id_o;
+                            $_SESSION["image"] = $image;
+                            $_SESSION["type"] = $type;
+                            $_SESSION["description"] = $description;
+                            if($type==$admin){
+                                $_SESSION["Admin"] = true;
+                                $_SESSION["User"] = false;
+                                header("location: AdminPanel.php");
+                            }else{
+                                $_SESSION["User"] = true;
+                                $_SESSION["Admin"] = false;
+                                header("location: index.php");
+                            };                             
+                            
+                            // Redirect user to welcome page
+                        } else{
+                            // Password is not valid, display a generic error message
+                            echo "Invalid username or password.";
+                        }
+                    }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    echo "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($con);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>URJET | Log In</title>
+  <script src="login.js"></script>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="index.php" class="h1"><b>UR</b>JET</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form name="login" onsubmit="return validateForm(event)" method="post">
+        <div class="input-group mb-3">
+          <input onblur="veriflogin()" onkeyup="veriflogin()" name="username" id="username" type="text" class="form-control" placeholder="Username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <p class="card bg-danger" id="errorusername"></p>
+        <div class="input-group mb-3">
+          <input onblur="veriflogin()" onkeyup="veriflogin()" name="password" id="password" type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <p class="card bg-danger" id="errorpassword"></p>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+        </div>
+      </form>
+      <p class=" float-right">
+        <a href="signup.php" class="text-center">Sign Up</a>
+      </p>
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
