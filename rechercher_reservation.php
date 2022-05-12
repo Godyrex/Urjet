f4f7b2a544bae3833ec1f0de93d3a0316f10ea4c
Ghadaa77 <80518452+Ghadaa77@users.noreply.github.com>
2022-05-12 13:58:58 +0200

    Add files via upload

--- rechercher_reservation.php
+++ rechercher_reservation.php
@@ -1,7 +1,6 @@
 <?php
 include '../config.php';
-$rows=NULL;
-
+$rows=0;
 if ($_POST ['submit'])
 {
     $key = $_POST['key'];
@@ -17,146 +16,145 @@ if ($_POST ['submit'])
 <!DOCTYPE HTML>
 
 <html>
-	<head>
-		<title>reservation</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-		<script src="finsert.js"></script>
-	</head>
-	<body class="is-preload">
+    <head>
+        <title>Rechercher une reservation</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+        <script src="finsert.js"></script>
+    </head>
+    <body class="is-preload">
 
-		<!-- Page Wrapper -->
-			<div id="page-wrapper">
+        <!-- Page Wrapper -->
+            <div id="page-wrapper">
 
-				<!-- Header -->
-					<header id="header">
-						<h1><a href="index.html">Urjet</a></h1>
-						<nav id="nav">
-							<ul>
-								<li class="special">
-									<a href="#menu" class="menuToggle"><span>Menu</span></a>
-									<div id="menu">
-										<ul>
-										<li><a href="index.php">Accueil (airplanes catalog)</a></li>
-											<li><a href="cata.php">Airplanes catalog</a></li>
-											<?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] && isset($_SESSION["Admin"]) && $_SESSION["Admin"] === true) { ?>
-											<li><a href="add.php">Avion</a></li>
-											<?php }?>
-											<li><a href="Resersation.php"></a>Réservation</li>
-											<li><a href="afficherListereclamation.php">Réclamation</a></li>
-											<li><a href="ajoutereponse.php">Réponse</a></li>
-											<li><a href="Maintence.php"></a>Afficher la liste des demandes</li>
-											<li><a href="Demande.php">Demande de maintenance</a></li>
-											<li><a href="Voyage.php">Voyage</a></li>
+                <!-- Header -->
+                    <header id="header">
+                        <h1><a href="index.html">Urjet</a></h1>
+                        <nav id="nav">
+                            <ul>
+                                <li class="special">
+                                    <a href="#menu" class="menuToggle"><span>Menu</span></a>
+                                    <div id="menu">
+                                        <ul>
+                                        <li><a href="index.php">Accueil (airplanes catalog)</a></li>
+                                            <li><a href="cata.php">Airplanes catalog</a></li>
+                                            <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] && isset($_SESSION["Admin"]) && $_SESSION["Admin"] === true) { ?>
+                                            <li><a href="add.php">Avion</a></li>
+                                            <?php }?>
+                                            <li><a href="ajouter_resersation.php"></a>Réservation</li>
+                                            <li><a href="afficherListereclamation.php">Réclamation</a></li>
+                                            <li><a href="ajouter_evenement.php">evenement</a></li>
+                                            <li><a href="maintance.php"></a>Afficher la liste des demandes</li>
+                                            <li><a href="demande.php">Demande de maintenance</a></li>
+                                            <li><a href="Voyage.php">Voyage</a></li>
                                             <?php if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) { ?>
                                             <li><a href="signup.php">Sign Up</a></li>
-											<li><a href="login.php">Log In</a></li>
+                                            <li><a href="login.php">Log In</a></li>
                                             <?php }?>
-											<?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] && isset($_SESSION["Admin"]) && $_SESSION["Admin"] === true) { ?>
+                                            <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] && isset($_SESSION["Admin"]) && $_SESSION["Admin"] === true) { ?>
                                                 <li><a href="AdminPanel.php">Admin Panel</a></li>
                                                 <?php }?>
                                             <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
                                             <li><a href="settings.php">Settings</a></li>
-											<li><a href="logout.php">Log out</a></li>
+                                            <li><a href="logout.php">Log out</a></li>
                                             <?php }?>
-										</ul>
-									</div>
-								</li>
-							</ul>
-						</nav>
-					</header>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
 
-				<!-- Main -->
-					<article id="main">
-						<header>
-							<h2>reservation</h2>
-							<p>Liste des evenements</p>
-						</header>
-						<section class="wrapper style5">
-							<div class="inner">
-                            <form action="rechercherreclamation.php" method="post" >
-										<div class="row gtr-uniform"> 
-											<div class="col-6 col-12-xsmall"> 
-												<input type="text" name="key"  placeholder="Recherche.." />
-												<input type="submit" name="submit"  value="chercher" />
-												
-											</div>  
-										</div>
-							</form>
-								<section>
-							
-		<table border="1" align="center">
-			<tr>
-				<th>idres</th>
-				<th>idclient</th>
-				<th>ideven</th>
-				<th>dateres</th>
-
+                <!-- Main -->
+                    <article id="main">
+                        <header>
+                            <h2>reservation</h2>
+                            <p>Liste des reservations</p>
+                        </header>
+                        <section class="wrapper style5">
+                            <div class="inner">
+                            <form action="rechercher_reservation.php" method="post" >
+                                        <div class="row gtr-uniform"> 
+                                            <div class="col-6 col-12-xsmall"> 
+                                                <input type="text" name="key"  placeholder="Recherche.." />
+                                                <input type="submit" name="submit"  value="chercher" />
+                                                
+                                            </div>  
+                                        </div>
+                            </form>
+                                <section>
+                            
+        <table border="1" align="center">
+            <tr>
+                <th>idres</th>
+                <th>idclient</th>
+                <th>ideven</th>
+                <th>dateres</th>
                 <th>etatres</th>
-				<th>Modifier</th>
-				<th>Supprimer</th>
-			</tr>
-			<?php
-				{ if ($rows!=0)
-                    {foreach ($results as $reservation) {
-			?>
-			<tr>
-				<td><?php echo $reservation['idres']; ?></td>
-				<td><?php echo $reservation['idclient']; ?></td>
-				<td><?php echo $reservation['ideven']; ?></td>
+                <th>Modifier</th>
+                <th>Supprimer</th>
+            </tr>
+            
+            <?php
+                { if ($rows!=0)
+                    {foreach ($results as $reservation ) {
+            ?>
+            <tr>
+                <td><?php echo $reservation['idres']; ?></td>
+                <td><?php echo $reservation['idclient']; ?></td>
+                <td><?php echo $reservation['ideven']; ?></td>
                 <td><?php echo $reservation['dateres']; ?></td>
                 <td><?php echo $reservation['etatres']; ?></td>
-				
-				
-				<td>
-					<form method="POST" action="modifier_reservation.php">
-						<input type="submit" name="Modifier" value="Modifier">
-						<input type="hidden" value=<?PHP echo $reservation['idres']; ?> name="idres">
-					</form>
-				</td>
-				<td>
-					<a href="supprimer_reservation.php?idres=<?php echo $reservation['idres']; ?>">Supprimer</a>
-				</td>
-			</tr>
+
+                <td>
+                    <form method="POST" action="modifier_reservation.php">
+                        <input type="submit" name="Modifier" value="Modifier">
+                        <input type="hidden" value=<?PHP echo $reservation['idres']; ?> name="idres">
+                    </form>
+                </td>
+                <td>
+                    <a href="supprimer_reservation.php?idres=<?php echo $reservation['idres']; ?>">Supprimer</a>
+                </td>
+            </tr>
             <?php
-			  }}}
-			?>
+              }}}
+            ?>
         
            
-			
-		</table>
-									
-								</section>
-							</div>
-						</section>
-					</article>
+            
+        </table>
+                                    
+                                </section>
+                            </div>
+                        </section>
+                    </article>
 
-				<!-- Footer -->
-					<footer id="footer">
-						<ul class="icons">
-							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-							<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-							<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-							<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
-							<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
-						</ul>
-						<ul class="copyright">
-							<li>&copy; Untitled</li><li>Design: <a href="http://urjet.com">HTML5 UP</a></li>
-						</ul>
-					</footer>
+                <!-- Footer -->
+                    <footer id="footer">
+                        <ul class="icons">
+                            <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                            <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                            <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                            <li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+                            <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+                        </ul>
+                        <ul class="copyright">
+                            <li>&copy; Untitled</li><li>Design: <a href="http://urjet.com">HTML5 UP</a></li>
+                        </ul>
+                    </footer>
 
-			</div>
+            </div>
 
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+        <!-- Scripts -->
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/jquery.scrollex.min.js"></script>
+            <script src="assets/js/jquery.scrolly.min.js"></script>
+            <script src="assets/js/browser.min.js"></script>
+            <script src="assets/js/breakpoints.min.js"></script>
+            <script src="assets/js/util.js"></script>
+            <script src="assets/js/main.js"></script>
 
-	</body>
+    </body>
 </html>
---
+++ trier_reservation.php
@@ -0,0 +1,399 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="Viewsport" content="width=device-width, initial-scale=1">
+  <title>Liste des Reservations</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Navbar Search -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+          <i class="fas fa-search"></i>
+        </a>
+        <div class="navbar-search-block">
+          <form class="form-inline">
+            <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </li>
+
+      <!-- Messages Dropdown Menu -->
+   
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">URJET</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Ghada Mejri</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Dashboard v1</p>
+                </a>
+              </li>
+             
+            </ul>
+          </li>
+          
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Gestion d'Utilisateur
+                <i class="fas fa-angle-left right"></i>
+                
+              </p>
+            </a>
+           
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-chart-pie"></i>
+              <p>
+                Gestion des Avions
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/charts/chartjs.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>ChartJS</p>
+                </a>
+             
+             
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tree"></i>
+              <p>
+                Gestion des Reservations
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="afficher_reservation" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Reservations</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="afficher_evenement.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>evenement</p>
+                </a>
+              </li>
+              
+              
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Gestion des Reservations
+
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/forms/general.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>General Elements</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/forms/advanced.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Advanced Elements</p>
+                </a>
+              </li>
+              
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Gestion de Maintenance
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/tables/simple.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Simple Tables</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/tables/data.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>DataTables</p>
+                </a>
+              </li>
+              
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Gestion de Reclamation
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/tables/reclamation.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Reclamation</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/tables/data.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Reponse</p>
+                </a>
+              </li>
+              
+            </ul>
+          </li>
+          
+          
+      
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+  <section class="col-6 col-lg-10 connectedSortable">
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Reservations</li>
+            </ol>
+          </div><!-- /.col -->
+          <div class="col-lg-6 col-5 text-right">
+              <a href="#" class="btn btn-sm btn-neutral">New</a>
+              <a href="afficher_reservation.php" class="btn btn-sm btn-neutral">Retourner sans tri</a>
+            </div>
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+
+            <!-- Card header -->
+            <div class="card-header border-0">
+              <h3 class="mb-0">La liste des Reservations :</h3>
+            </div>
+            <!-- Light table -->
+            <div class="table-responsive">
+              <table class="table align-items-center table-flush">
+                <thead class="thead-light">
+                  <tr>
+                    <th scope="col" class="sort" data-sort="name">id res</th>
+                    <th scope="col" class="sort" data-sort="budget">id client</th>
+                    <th scope="col" class="sort" data-sort="status">ideven</th>
+                    <th scope="col" class="sort" data-sort="completion">dateres</th>
+                    <th scope="col" class="sort" data-sort="completion">etatres</th>
+                   
+                     <th scope="col"></th>
+                  </tr>
+                </thead>
+<tbody>
+<?php //on inclut notre fichier de connection $pdo = Database::connect(); //on se connecte à la base $sql = 'SELECT * FROM user ORDER BY id DESC'; //on formule notre requete foreach ($pdo->query($sql) as $row) { //on cree les lignes du tableau avec chaque valeur retournée
+                           include "../Controller/reservationC.php";
+                           $reservation = new reservationC();
+                           $resultat=$reservation->trier_reservation();
+
+                           echo '
+<br />
+<tr>';
+foreach($resultat as $row) {                         
+echo'
+
+<td>' . $row['idres'] . '</td>
+<p>
+';
+      echo'
+	  
+	  <td>' . $row['idclient'] . '</td>
+	  <p>
+	  ';
+								  echo'
+                            
+    <td>' . $row['ideven'] . '</td>
+        <p>';
+            echo'
+                            
+      <td>' . $row['dateres'] . '</td>
+       <p>';
+
+        echo'
+                            
+        <td>' . $row['etatres'] . '</td>
+        <p> ';
+
+      
+
+
+          echo '<td>';
+         echo '<a class="btn btn-success" href="modifier_reservation.php?idres=' . $row['idres'] . '">Update</a>';// un autre td pour le bouton d'update
+       
+                      echo '<a class="btn btn-primary" href="Supprimer_reservation.php?idres=' . $row['idres'] . ' ">Delete</a>';// un autre td pour le bouton de suppression
+     
+         echo '</td>
+        <p>';
+       echo '</tr>
+       <p>
+       ';
+          
+}
+      
+                                                    ?>  
+</tbody>
+
+        </div>
+  </div>
+ 
+  
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+
+</body>
+</html>           
